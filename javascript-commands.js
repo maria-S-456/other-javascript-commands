da11c3b1f576966be02a234d987afcd33296f59e
maria-S-456 <mars381pika@gmail.com>
2016-11-30 01:40:44 -0500

    first commit

---
+++ javascript-commands.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName('intro-heading')[0];
+document.getElementsByClassName('intro-heading')[0].innerHTML;
+document.getElementsByClassName('intro-heading')[0].style.color="red";
+document.getElementsByTagName('div');
+document.getElementById('first');
